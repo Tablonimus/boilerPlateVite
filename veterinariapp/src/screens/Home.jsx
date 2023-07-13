fdd215acb26d2c1fa0af2bc09fc6a04e14c05769
tablonimus <lautamartin1995@gmail.com>
2023-07-13 00:13:09 -0300

    trabajo

--- veterinariapp/src/screens/Home.jsx
+++ veterinariapp/src/screens/Home.jsx
@@ -3,5 +3,5 @@ import React from "react";
 export default function Home() {
   console.log("estoy en home");
 
-  return <div className="text-xl text-red-500 font-bold underline">Esto le toca a henry</div>;
+  return <div className="text-xl text-red-500 font-bold underline">Esto le toca a henry, ahora hago un segundo aporte</div>;
 }
